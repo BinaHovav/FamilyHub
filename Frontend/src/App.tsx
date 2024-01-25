f802b73319d340e0fd26f197a97894f71422d731
Bina Hovav <binahovav@gmail.com>
2024-01-25 08:31:13 +0200

    remove a line not in use

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
-import { useState } from 'react'
-
 import Homepage from './pages/homepage/Homepage'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Homepage />
